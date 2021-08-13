9552c24bd62004583fd3ac2d3ee97d546110f88e
sjtgshivam <getlovebymagicalshivam@gmail.com>
2021-08-13 23:21:47 +0530

    feat: translate query funcrion added

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const inputTextArea = document.querySelector("#input-text-area");
 const outputDiv = document.querySelector("#output-div");
 const translateButton = document.querySelector("#translate-btn");
 const minionApi = "https://api.funtranslations.com/translate/minion.json";
+
+const translateQuery = (text) => {
+  return minionApi + "?text=" + text;
+};
+
 const transelateText = () => {};
 
 translateButton.addEventListener("click", transelateText);
