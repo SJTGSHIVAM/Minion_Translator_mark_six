79795fb95c5a082d2dd08dcb5a9f971a3bdb7519
sjtgshivam <getlovebymagicalshivam@gmail.com>
2021-08-13 23:19:32 +0530

    feat: EventListener added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const inputTextArea = document.querySelector("#input-text-area");
 const outputDiv = document.querySelector("#output-div");
 const translateButton = document.querySelector("#translate-btn");
+const minionApi = "https://api.funtranslations.com/translate/minion.json";
+const transelateText = () => {};
+
+translateButton.addEventListener("click", transelateText);
