adfddd2bbf665cd231c8a3e8bc86154577ced9a3
sjtgshivam <getlovebymagicalshivam@gmail.com>
2021-08-13 23:31:12 +0530

    feat: translate Text function added

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const errorHandler = (err) => {
   alert("Something went wrong, Please try again later!");
 };
 
-const transelateText = () => {};
+const transelateText = () => {
+  const textToTranslate = inputTextArea.value;
+  fetch(translateQuery(textToTranslate))
+    .then((res) => res.json())
+    .then((json) => {
+      const tranlatedText = json.contents.translated;
+      outputDiv.innerText = tranlatedText;
+    })
+    .catch(errorHandler);
+};
 
 translateButton.addEventListener("click", transelateText);
